c93c3dcce06113f2ff1afe6ba733a432db862263
Striker <65626392+StrikerITA@users.noreply.github.com>
2022-04-05 14:53:42 +0200

    Rimozione test prenotazione
    - test.html (inutile) cartella prenotazione
    - spazi inutili
    - commenti di codice inutilizzato

--- prenotazione/Prenotazioni.php
+++ prenotazione/Prenotazioni.php
@@ -23,63 +23,49 @@
 
                 $dateEnd = date_modify($dateEnd, '+7 days');
                 $DataPrenotazione = date_format($dateEnd, 'Y/m/d');
-                
-        
-                
+                       
+                $StatoPrenotazione = "attesa";
+                    
+                // Creazione connessione
+                $connessione = new mysqli($servername, $username, $password, $dbname);
+                    
+                // Check connection
+                if ($connessione->connect_error) {
+                    die("Connection failed: " . $connessione->connect_error);
+                }
+                    
+                $sql_copie = "SELECT CopieDisponibili FROM libro WHERE CodiceLibro = '$CodiceLibro'";
+                $result = $connessione->query($sql_copie);
+                $row = $result->fetch_assoc();
 
-                //dati utente
-                //if(!empty($_POST["CodiceLibro"])){
-                    
-                    
-                    $StatoPrenotazione = "attesa";
-                    //$CodiceLibro = $_POST["codiceLibro"];
-                    echo $CodiceLibro;
-                    
-                    // Creazione connessione
-                    $connessione = new mysqli($servername, $username, $password, $dbname);
-                    
-                    // Check connection
-                    if ($connessione->connect_error) {
-                        die("Connection failed: " . $connessione->connect_error);
+                if($row["CopieDisponibili"] > 0){
+                    $sql = "INSERT INTO prenota (DataPrenotazione, CodiceFiscale, CodiceLibro, StatoPrenotazione) VALUES ('$DataPrenotazione', '$CodiceFiscale', '$CodiceLibro', '$StatoPrenotazione')";
+
+                    //inserimento nel database
+                    if($connessione->query($sql) === TRUE) {
+                        //alert("Prenotazione aggiunta con successo");
+                    } else {
+                        echo "Errore nella prenotazione, il libro non è stato prenotato";
                     }
-                    
-                    $sql_copie = "SELECT CopieDisponibili FROM libro WHERE CodiceLibro = '$CodiceLibro'";
-                    $result = $connessione->query($sql_copie);
-                    $row = $result->fetch_assoc();
 
-                    if($row["CopieDisponibili"] > 0){
-                        $sql = "INSERT INTO prenota (DataPrenotazione, CodiceFiscale, CodiceLibro, StatoPrenotazione) VALUES ('$DataPrenotazione', '$CodiceFiscale', '$CodiceLibro', '$StatoPrenotazione')";
+                    $num_copie = $row["CopieDisponibili"]; // sottrae le copie
+                    $num_copie = $num_copie - 1;
+                    $sql = "UPDATE libro SET CopieDisponibili = '$num_copie' WHERE CodiceLibro = '$CodiceLibro'";
+                    //cambia il record nel database
 
-                        //inserimento nel database
-                        if($connessione->query($sql) === TRUE) {
-                            //alert("Prenotazione aggiunta con successo");
-                            //messaggio spostato dopo l'aggiornamento copie
-                        } else {
-                            echo "Errore nella prenotazione, il libro non è stato prenotato";
-                        }
-
-                        //NON CONVERTE L'OBJ IN STRING E DA STRING IN INT
-                        $num_copie = $row["CopieDisponibili"]; // sottrae le copie
-                        $num_copie = $num_copie - 1;
-                        $sql = "UPDATE libro SET CopieDisponibili = '$num_copie' WHERE CodiceLibro = '$CodiceLibro'";
-
-                        //cambia il record nel database
-                        if($connessione->query($sql) === TRUE){
-                            //ha aggiornato le copie e fa un alert 
-                            //alert("Prenotazione aggiunta con successo");
-                        }else{
-                            echo "Impossibile aggiornare le copie disponibili!";
-                        }
-
+                    if($connessione->query($sql) === TRUE){
+                        //alert("Prenotazione aggiunta con successo");
                     }else{
-                        alert("Non ci sono copie disponibili del libro selezionato!");
+                        echo "Impossibile aggiornare le copie disponibili!";
                     }
 
+                }else{
+                    alert("Non ci sono copie disponibili del libro selezionato!");
+                }
                         
-                    alertRedirect("Deve ritirare il libro entro il ".date_format($dateEnd, 'd/m/y'), "/Biblioteca_polizzi/index.php");
-                    //header('Location: /Biblioteca_polizzi/index.php');
-                    $connessione->close();
-                //}
+                alertRedirect("Deve ritirare il libro entro il ".date_format($dateEnd, 'd/m/y'), "/Biblioteca_polizzi/index.php");
+                //header('Location: /Biblioteca_polizzi/index.php');
+                $connessione->close();
             }
         ?>
     </body>
